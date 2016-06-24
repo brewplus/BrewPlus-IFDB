4a899606141bdc9e1876d7a399c6639ce963e317
girella <giacomo.girella@gmail.com>
2016-06-24 16:29:24 +0200

    Aggiunta classe per le costanti

---
+++ brewplus/brewplus-ifdb/src/main/java/jmash/interfaces/Constants.java
@@ -0,0 +1,9 @@
+package jmash.interfaces;
+
+public interface Constants {
+    
+    static final String GRANI = "grani";
+    static final String FIOCCHI = "fiocchi";
+    static final String CHICCHI = "chicchi";
+    
+}
